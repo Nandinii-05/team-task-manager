153dc521592a27a9e16b7cefc6408d8015a73ad9
Nandinii-05 <singhkhushi262605@gmail.com>
2026-05-02 15:31:18 +0530

    Fix port log

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const mongoose = require("mongoose");
