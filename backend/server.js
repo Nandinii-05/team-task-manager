39f8d1263509c861747b7f267a40131f4835e32a
Nandinii-05 <singhkhushi262605@gmail.com>
2026-05-02 14:40:03 +0530

    Fix port log

--- backend/server.js
+++ backend/server.js
@@ -88,8 +88,10 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // start server
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 
