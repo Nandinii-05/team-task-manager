f4223514c7f3942a6062655b24335ef47d144269
Nandinii-05 <singhkhushi262605@gmail.com>
2026-05-02 15:43:27 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "backend", "views"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
